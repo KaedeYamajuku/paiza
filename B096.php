f9a3b8f9e29a6f1613d882824cae8019b29bb7b4
KaedeYamajuku <kaede.yamajuku@asia-quest.jp>
2023-11-01 17:10:31 +0900

    B096

---
+++ B096.php
@@ -0,0 +1,36 @@
+<?php
+[$line, $column] = explode(" ", trim(fgets(STDIN)));
+
+//爆弾がある行数 * 全体の列数
+// + 爆弾がある列数* (全体の行数 - 爆弾がある行数)
+// - 爆弾が複数個ある行数 * 全体の列数 * (その列の爆弾の数 - 1)
+// - 爆弾が複数個ある列数 * (全体の行数 - 爆弾がある行数) * (その列の爆弾の数 - 1)
+
+
+//各行の処理
+$includeBombLine = 0;
+
+for ($i = 0; $i < $line; $i++) {
+    $eachLine[] = str_split(trim(fgets(STDIN)));
+
+    if (in_array('#', $eachLine[$i])) {
+        $includeBombLine++;
+        //$includeBombLine = 爆弾がある行数
+    }
+}
+
+//各列の処理
+$includeBombColumn = 0;
+
+for ($i = 0; $i < $column; $i++) {
+    $eachColumn[] = array_column($eachLine, $i);
+
+    if (in_array('#', $eachColumn[$i])) {
+        $includeBombColumn++;
+        //$includeBombColumn = 爆弾がある列数
+    }
+}
+
+$answer = $includeBombLine * $column + $includeBombColumn * ($line - $includeBombLine);
+
+echo $answer;
--- C083.php
+++ C083.php
@@ -3,23 +3,23 @@
 [$dataCount, $multiple] = explode(" ", trim(fgets(STDIN)));
 
 for ($i = 0; $i < $dataCount; $i++) {
-     //各年度の売り上げを取得する
-     $eachYearSales = trim(fgets(STDIN));
-     //各年度のグラフの横幅を求める
-     $eachGraphCount[] = $eachYearSales / $multiple;
+    //各年度の売り上げを取得する
+    $eachYearSales = trim(fgets(STDIN));
+    //各年度のグラフの横幅を求める
+    $eachGraphCount[] = $eachYearSales / $multiple;
 }
 
 //グラフの横幅の最大値を求める
 $graphCount = max($eachGraphCount);
 
-foreach ($eachGraphCount as $index => $n){
-    echo ($index + 1). ':';
+foreach ($eachGraphCount as $index => $n) {
+    echo ($index + 1) . ':';
     //記号を$eachGraphCountの値に沿って出力する
     for ($i = 0; $i < $n; $i++) {
-         echo '*';
+        echo '*';
     }
     for ($i = 0; $i < $graphCount - $n; $i++) {
-         echo '.';
+        echo '.';
     }
     echo PHP_EOL;
 }
