a9900854d5c11b6de693ddf30dcaa2c60ad4cb84
KaedeYamajuku <kaede.yamajuku@asia-quest.jp>
2023-11-02 10:59:35 +0900

    一部文章の並べ替えのみ

--- B096.php
+++ B096.php
@@ -5,26 +5,28 @@
 // + 爆弾がある列数* (全体の行数 - 爆弾がある行数)
 
 //各行の処理
+//$includeBombLine = 爆弾がある行数
 $includeBombLine = 0;
 
 for ($i = 0; $i < $line; $i++) {
+    //各行の文字列を取得
     $eachLine[] = str_split(trim(fgets(STDIN)));
 
     if (in_array('#', $eachLine[$i])) {
         $includeBombLine++;
-        //$includeBombLine = 爆弾がある行数
     }
 }
 
 //各列の処理
+//$includeBombColumn = 爆弾がある列数
 $includeBombColumn = 0;
 
 for ($i = 0; $i < $column; $i++) {
+    //各列の文字列を取得
     $eachColumn[] = array_column($eachLine, $i);
 
     if (in_array('#', $eachColumn[$i])) {
         $includeBombColumn++;
-        //$includeBombColumn = 爆弾がある列数
     }
 }
 
